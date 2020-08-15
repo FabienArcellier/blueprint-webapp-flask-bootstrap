e9773796d059ab37300741b5e1572d8779aa3132
Fabien Arcellier <fabien.arcellier@gmail.com>
2020-08-15 14:40:00 +0200

    * configure run in production using gunicorn

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='blueprint-webapp-flask',
+    name='blueprint-webapp-flask-bootstrap',
     version='1.0.0',
     packages=find_packages(exclude=["*_tests"]),
     license='MIT',
     long_description=open('README.md').read(),
-    install_requires=[
+    install_requires = [
         'flask',
         'gunicorn',
         'honcho'
